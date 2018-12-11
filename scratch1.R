bef9dae2773d03530847fd3761aa6306c3a56226
tastyCanOfMalk <eddytheflow@gmail.com>
2018-12-11 13:45:55 -0500

    bins binwidth calcs

--- scratch1.R
+++ scratch1.R
@@ -1,9 +1,17 @@
+
+bw.Served <- diff(range(x$Served)) / (2 * IQR(x$Served) / length(x$Served)^(1/3))
+
+x %>% 
+  filter(Race != "Other") %>% 
+  ggplot(aes(x=Served,fill=Race))+
+  geom_histogram(bins=bw.Served)+
+  facet_wrap(Race~.)
+
 # shows an interesting large peak around 7.5 years
 x %>% 
   filter(Race != "Other") %>% 
-  mutate(Served = Age-AgeWhenReceived) %>% 
   ggplot(aes(x=Served,y=Race,fill=Race))+
-  stat_binline(bins=10)+
+  stat_binline(bins=bw.Served)+
   scale_x_continuous(limits=c(0,40),
                      breaks=c(seq(0,40,5)))
 
@@ -11,19 +19,16 @@ x %>%
 # harsher punishments/quicker death for more victims killed?
 glimpse(x)
 x %>% 
-  mutate(Served = Age-AgeWhenReceived) %>% 
   mutate(VictimsTotal = FemaleVictim+MaleVictim) %>% 
   count(VictimsTotal)
 # Graph victimstotal < 5 to keep clean
 x %>% 
-  mutate(Served = Age-AgeWhenReceived) %>% 
   mutate(VictimsTotal = as.factor(FemaleVictim+MaleVictim)) %>% 
   filter(as.numeric(VictimsTotal) < 5) %>% 
   ggplot(aes(x=Served,y=VictimsTotal))+
   geom_density_ridges()
 
 x %>% 
-  mutate(Served = Age-AgeWhenReceived) %>% 
   mutate(VictimsTotal = as.factor(FemaleVictim+MaleVictim)) %>% 
   filter(as.numeric(VictimsTotal) < 5) %>% 
   ggplot(aes(x=Served,y=Race))+
@@ -31,9 +36,3 @@ x %>%
 
 glimpse(xx)
 
-x %>% 
-  filter(Race != "Other") %>% 
-  mutate(Served = Age-AgeWhenReceived) %>% 
-  ggplot(aes(x=Served,fill=Race))+
-  geom_histogram()+
-  facet_wrap(Race~.)
