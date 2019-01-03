9844b9303d499ae235b0390d8f0305e2d43483d7
tastyCanOfMalk <eddytheflow@gmail.com>
2019-01-03 14:05:57 -0500

    fill missing values

---
+++ scratch2.R
@@ -0,0 +1,26 @@
+# Fill in missing values
+
+## NumberVictim
+### Use median, only 18 NA's
+x <- x %>% 
+  mutate(NumberVictim = replace(NumberVictim,
+                                is.na(NumberVictim),
+                                1))
+summary(x$NumberVictim)
+
+summary(x$Codefendants)
+
+lm1 <- lm(Codefendants~Race+AgeWhenReceived,data=x)
+summary(lm1)
+
+x.black <- subset(x,Race=="Black")
+x.white <- subset(x,Race=="White")
+x.hispa <- subset(x,Race=="Hispanic")
+summary(x.black$Codefendants)
+summary(x.white$Codefendants)
+summary(x.hispa$Codefendants)
+# looks like we could either assign codefendent values based on race
+# or use the linear model to be more specific, however, getting specific might
+# be a waste of time as 1.4 defendants would be the same as 0.8
+# median or mean?
+
