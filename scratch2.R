d37c45604c08a4efddd9dd611c41f33143f9f59d
tastyCanOfMalk <eddytheflow@gmail.com>
2019-01-04 14:17:42 -0500

    missing data fill

--- scratch2.R
+++ scratch2.R
@@ -1,3 +1,9 @@
+# corrplot to start with?
+glimpse(x)
+x.pairs <- x %>% 
+  select(Age,Race,AgeWhenReceived,EducationLevel,NativeCounty,
+         PreviousCrime,Codefendants,NumberVictim)
+ggpairs(x.pairs)
 # Fill in missing values
 
 ## NumberVictim
@@ -5,9 +11,43 @@
 x <- x %>% 
   mutate(NumberVictim = replace(NumberVictim,
                                 is.na(NumberVictim),
-                                1))
-summary(x$NumberVictim)
+                                1)) %>% 
+  mutate(Codefendants = replace(Codefendants,
+                                is.na(Codefendants),
+                                1)) %>% 
+  mutate(EducationLevel = replace(EducationLevel,
+                                  is.na(EducationLevel),
+                                  10)) %>% 
+  mutate(PreviousCrime = replace(PreviousCrime,
+                                  is.na(PreviousCrime),
+                                  1))
+  
+x %>% 
+  count(PreviousCrime)
 
+# https://www.r-bloggers.com/imputing-missing-data-with-r-mice-package/
+install.packages("mice")
+library(mice)
+md.pattern(x.pairs)
+install.packages("VIM")
+library(VIM)
+aggr_plot <- aggr(x.pairs, col=c('navyblue','red'), 
+                  numbers=TRUE, 
+                  sortVars=TRUE, 
+                  labels=names(data), 
+                  cex.axis=.7, 
+                  gap=3, 
+                  ylab=c("Histogram of missing data","Pattern"))
+marginplot(x.pairs[c(1,3)])
+marginplot(x.pairs[c(1,6)])
+glimpse(x.pairs)
+
+
+
+
+summary(x$EducationLevel)
+summary(x$PreviousCrime)
+summary(x$NumberVictim)
 summary(x$Codefendants)
 
 lm1 <- lm(Codefendants~Race+AgeWhenReceived,data=x)
@@ -23,4 +63,4 @@ summary(x.hispa$Codefendants)
 # or use the linear model to be more specific, however, getting specific might
 # be a waste of time as 1.4 defendants would be the same as 0.8
 # median or mean?
-
+# use tree?
