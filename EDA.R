5d1331e64a5d116114cfe50df36fb310b8b9a336
tastyCanOfMalk <eddytheflow@gmail.com>
2019-01-07 14:06:17 -0500

    wording

--- EDA.R
+++ EDA.R
@@ -11,8 +11,6 @@ library(viridis)
 if(!require(gridExtra)) install.packages("gridExtra")
 library(gridExtra)
 
-# Fill in missing values as much as possible
-
 setwd("/home/e/R/inmate.last.words")
 # setwd("C:/Users/e/Documents/R/inmate.last.words")
 x <- read_csv("data/Texas Last Statement - CSV.csv")
@@ -33,14 +31,12 @@ gg_miss_which(x)
 # summary(x)
 
 # Fill in some missing values
-## I want to add an x$Served column but this requires 
-##  complete data on AgeWhenReceived
+## I want to add a $Served column but this requires complete data on AgeWhenReceived
 x %>% 
   select(Race, AgeWhenReceived) %>% 
   na.omit() %>% 
   ggplot(aes(x=AgeWhenReceived,fill=Race)) +
   geom_density(aes(alpha=.5))
-
 
 ### Calculate mean AWR for each Race and replace NA's with that
 x %>% 
@@ -79,6 +75,7 @@ nums <- which(is.na(x$NumberVictim))
 nums1 <- x[nums,]
 
 ## White vs Black/Hispanic have somewhat differing values, fill in based on mean
+
 
 # EDA, focus on demographic
 x <- x %>% 
